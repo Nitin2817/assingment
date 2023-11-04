81ce2adce6ccfc35f87106895b7fd78680183ebc
Nitin2817 <149860483+Nitin2817@users.noreply.github.com>
2023-11-04 11:53:28 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int arr[5];
+   int max;
+   printf("Enter five integers:\n");
+   for(int i=0;i<5;i++){
+    scanf("%d",&arr[i]);
+   }
+   max=arr[0];
+   for(int i=1;i<5;i++){
+    if(arr[i]>max){
+        max=arr[i];
+    }
+   }
+   printf("The maximum number in the array is: %d\n",max);
+   return 0;
+}
